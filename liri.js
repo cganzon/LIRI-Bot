aed90bbfde25e3a65db741287d2512d87dcecb32
cganzon <crganzon@gmail.com>
2019-03-17 20:53:48 -0700

    movie-this function

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 // Importing the keys.js file
 var keys = require("./keys.js");
+// Grabbing axios package
+var axios = require("axios");
 
 // Grabbing user input
 var command = process.argv[2];
@@ -17,7 +19,20 @@ if (command === "spotify-this-song") {
 };
 
 if (command === "movie-this") {
-
+    axios
+        .get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            // console.log(response.data);
+            console.log("Movie: " + response.data.Title);
+            console.log("Year Released: " + response.data.Year);
+            console.log(response.data.Ratings[0].Source + " Score: " + response.data.Ratings[0].Value);
+            console.log(response.data.Ratings[1].Source + " Score: " + response.data.Ratings[1].Value);
+            console.log("Country produced: " + response.data.Country);
+            console.log("Language(s): " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+    );
 };
 
 if (command === "do-what-it-says") {
