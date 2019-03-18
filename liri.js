93f328ae5c888144815b7cddddca0055790ae6a6
cganzon <crganzon@gmail.com>
2019-03-17 20:36:11 -0700

    initial file setup

---
+++ liri.js
@@ -0,0 +1,25 @@
+// Reads and sets any environment variables with the dotenv package:
+require("dotenv").config();
+
+// Importing the keys.js file
+var keys = require("./keys.js");
+
+// Grabbing user input
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+if (command === "concert-this") {
+
+};
+
+if (command === "spotify-this-song") {
+
+};
+
+if (command === "movie-this") {
+
+};
+
+if (command === "do-what-it-says") {
+
+};
